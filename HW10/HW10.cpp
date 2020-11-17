af6e6a56cddef436c730285ac55eea8e86ddc8d2
Allist <ccma750@gmail.com>
2020-11-18 00:56:14 +0900

    FIN maybe...

--- HW10/HW10.cpp
+++ HW10/HW10.cpp
@@ -20,6 +20,7 @@ void DoIDFT(const cv::Mat&);
 void DFTofFabrics(string);
 
 cv::Mat sample_vec[SAMPLE_CNT];
+cv::Mat dft_mags[SAMPLE_CNT];
 
 void DoDFT(string input_path, cv::Mat &magMat, double scale_factor, bool printDFT, bool printIDFT, double x_ratio = 0.0, double y_ratio = 0.0)
 {
@@ -27,17 +28,20 @@ void DoDFT(string input_path, cv::Mat &magMat, double scale_factor, bool printDF
     cv::Mat dftImg;
     cv::Mat dftImgArr[2];
 
-    // CV_8UC1 to CV_32FC1
-    int x = inputImg.cols * x_ratio;
-    int y = inputImg.rows * y_ratio;
-    int crop_size;
+    int rect_size;
     if(inputImg.cols < inputImg.rows)
-        crop_size = inputImg.cols;
+        rect_size = inputImg.cols;
     else
-        crop_size = inputImg.rows;
-    crop_size *= scale_factor;
+        rect_size = inputImg.rows;
+    inputImg = inputImg(cv::Rect(0, 0, rect_size, rect_size));
+
+    // CV_8UC1 to CV_32FC1
+    int x = rect_size * x_ratio;
+    int y = rect_size * y_ratio;
+    int crop_size = rect_size * scale_factor;
+
     inputImg = inputImg(cv::Rect(x, y, crop_size, crop_size));
-    //cv::resize(inputImg, inputImg, cv::Size(CompN, CompN));
+    cv::resize(inputImg, inputImg, cv::Size(CompN, CompN));
     inputImg.convertTo(inputImg, CV_32FC1);
 
     // Do DFT
@@ -60,7 +64,7 @@ void DoDFT(string input_path, cv::Mat &magMat, double scale_factor, bool printDF
     
     // Extract half
     //cv::log(magMat, magMat);
-    cv::resize(magMat, magMat, cv::Size(CompN, CompN));
+    //cv::resize(magMat, magMat, cv::Size(CompN, CompN));
     //magMat = magMat.row(CompN/2);
 
     // Extract Triangle matrix. where x + y < CompN
@@ -69,6 +73,7 @@ void DoDFT(string input_path, cv::Mat &magMat, double scale_factor, bool printDF
     for(int y = 0; y < CompN; ++y) {
         cv::hconcat(opMagMat, magMat(cv::Rect(0, y, CompN-y, 1)), opMagMat);
     }
+    opMagMat.copyTo(magMat);
     */
 }
 
@@ -111,6 +116,32 @@ void DoIDFT(const cv::Mat &dftMat)
     cv::waitKey(0);
 }
 
+void SaveMagAsImg(const cv::Mat &saveImg, int idx)
+{
+    cv::Mat dftPrint;
+    saveImg.copyTo(dftPrint);
+
+    cv::log(dftPrint, dftPrint);
+
+    // rearrange
+    int halfX = dftPrint.cols/2;
+    int halfY = dftPrint.rows/2;
+    cv::Mat tmp;
+    // swap 1st quadrant and 3rd quadrant
+    dftPrint(cv::Rect(0, 0, halfX, halfY)).copyTo(tmp);
+    dftPrint(cv::Rect(halfX, halfY, halfX, halfY)).copyTo(dftPrint(cv::Rect(0, 0, halfX, halfY)));
+    tmp.copyTo(dftPrint(cv::Rect(halfX, halfY, halfX, halfY)));
+    // swap 2nd quadrant and 4th quadrant
+    dftPrint(cv::Rect(0, halfY, halfX, halfY)).copyTo(tmp);
+    dftPrint(cv::Rect(halfX, 0, halfX, halfY)).copyTo(dftPrint(cv::Rect(0, halfY, halfX, halfY)));
+    tmp.copyTo(dftPrint(cv::Rect(halfX, 0, halfX, halfY)));
+
+    // Print
+    cv::normalize(dftPrint, dftPrint, 0, 255, cv::NORM_MINMAX, CV_8UC1);
+    string path = "/home/ccma750/VM_Shared_Directory/수치해석_HW10_Images/DFT_Real_Fabrics/" + to_string(idx) + ".jpg";
+    cv::imwrite(path, dftPrint);
+}
+
 void DFTofFabrics(string dir_path)
 {
     cv::Mat magMat;
@@ -121,7 +152,57 @@ void DFTofFabrics(string dir_path)
     int idx;
     int sample_idx = 0;
     for(const auto & entry : fs::directory_iterator(dir_path)) {
-        
+        op_dist = DBL_MAX;
+        idx = 0;
+        cv::Mat avgVec = cv::Mat::zeros(1, 2*CompN*CompN/4/4, CV_32FC1);
+        cv::Mat retVec[4];
+        for(int i = 25; i <= 50; i += 5) {
+            double scale = (double)i/100;
+            DoDFT(entry.path().generic_string(), retVec[0], scale, false, false, 0.0, 0.0);
+            DoDFT(entry.path().generic_string(), retVec[1], scale, false, false, scale, 0.0);
+            DoDFT(entry.path().generic_string(), retVec[2], scale, false, false, 0.0, scale);
+            DoDFT(entry.path().generic_string(), retVec[3], scale, false, false, scale, scale);
+
+            for(int j = 0; j < 4; ++j) {
+                retVec[j] = retVec[j].rowRange(0, CompN/2);
+                // 1/8 of DFTs...
+                cv::hconcat(retVec[j](cv::Rect(0, 0, CompN/4, CompN/4)), retVec[j](cv::Rect(3*CompN/4, 0, CompN/4, CompN/4)), retVec[j]);
+                /* Triangle of DFTs...
+                for(int k = 0; k < CompN/2; ++k) {
+                    for(int l = 0; l < k; ++l) {
+                        retVec[j].at<float>(k, CompN/2-l) = 0.0;
+                        retVec[j].at<float>(k, CompN/2+l) = 0.0;
+                    }
+                }
+                */
+                retVec[j] = retVec[j].reshape(0, 1);
+                //retVec[j] = retVec[j].colRange(1, retVec[j].cols); // Remove DC
+                retVec[j].at<float>(0, 0) = 0.0;
+                avgVec = avgVec + retVec[j];
+            }
+            avgVec = avgVec / 4;
+            dist = cv::norm(retVec[0], avgVec, cv::NORM_L2);
+            if(dist < op_dist) {
+                avgVec.copyTo(sample_vec[sample_idx]);
+                op_dist = dist;
+                idx = i;
+            }
+        }
+        /*
+        avgVec = cv::Mat::zeros(CompN, CompN, CV_32FC1);
+        double scale = (double)idx/100;
+        DoDFT(entry.path().generic_string(), retVec[0], scale, false, false, 0.0, 0.0);
+        DoDFT(entry.path().generic_string(), retVec[1], scale, false, false, scale, 0.0);
+        DoDFT(entry.path().generic_string(), retVec[2], scale, false, false, 0.0, scale);
+        DoDFT(entry.path().generic_string(), retVec[3], scale, false, false, scale, scale);
+        for(int j = 0; j < 4; ++j) {
+            avgVec = avgVec + retVec[j];
+        }
+        avgVec.copyTo(dft_mags[sample_idx]);
+        SaveMagAsImg(dft_mags[sample_idx], sample_idx);
+        */
+
+        /*
         op_dist = DBL_MAX;
         idx = 0;
         for(int i = 10; i <= 50; i += 5) {
@@ -143,6 +224,7 @@ void DFTofFabrics(string dir_path)
                 idx = i;
             }
         }
+        */
         
 
         /*
@@ -223,7 +305,7 @@ void PredictPatterns(string dir_path)
 
     std::random_device rd;
     std::mt19937_64 gen(rd());
-    std::uniform_real_distribution<double> re_gen(0.3, 0.5);
+    std::uniform_real_distribution<double> re_gen(0.25, 0.5);
 
     for(const auto & entry : fs::directory_iterator(dir_path)) {
         for(int i = 0; i < 5; ++i) {
@@ -233,8 +315,21 @@ void PredictPatterns(string dir_path)
             scale_factor = re_gen(gen);
             DoDFT(entry.path().generic_string(), magMat, scale_factor, false, false, x_ratio, y_ratio);
 
+            magMat = magMat.rowRange(0, CompN/2);
+            // 1/8 of DFTs...
+            cv::hconcat(magMat(cv::Rect(0, 0, CompN/4, CompN/4)), magMat(cv::Rect(3*CompN/4, 0, CompN/4, CompN/4)), magMat);
+            /* Triangle of DFTs...
+            for(int k = 0; k < CompN/2; ++k) {
+                    for(int l = 0; l < k; ++l) {
+                        magMat.at<float>(k, CompN/2-l) = 0.0;
+                        magMat.at<float>(k, CompN/2+l) = 0.0;
+                    }
+            }
+            */
             magMat = magMat.reshape(0, 1);
-            magMat = magMat.colRange(1, magMat.cols); // Remove DC
+            
+            //magMat = magMat.colRange(1, magMat.cols); // Remove DC
+            magMat.at<float>(0, 0) = 0.0;
             //cv::log(magMat, magMat); // log
 
             predict_idx = -1;
@@ -272,7 +367,7 @@ int main(int argc, char **argv)
     cout << "Start DFT of Sample Patterns" << endl;
     DFTofFabrics(input_path);
 
-    
+
     cout << "Start Predict!" << endl;
     PredictPatterns(input_path);
     
